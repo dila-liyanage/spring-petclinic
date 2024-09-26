7aa917cd60803cd987198c2f1bf7dedc0747d617
Dilshan Liyanage <aliyana4@my.centennialcollege.ca>
2024-09-26 10:49:33 -0400

    Create TodoItem.java

---
+++ src/main/java/org/springframework/samples/petclinic/model/TodoItem.java
@@ -0,0 +1,10 @@
+package org.springframework.samples.petclinic.model;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+
+    public string name {get; set;}
+
+    public bool IsComplete {get; set;}
+}
